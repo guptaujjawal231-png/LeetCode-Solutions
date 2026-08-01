0e6698937d9c70557fc5a82117517bb5abe5bcaa
Ujjawal Gupta <guptaujjawal231@gmail.com>
2026-08-01 12:21:15 +0530

    Time: 0 ms (100%), Space: 19.4 MB (31.78%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,20 +1,27 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        if n==1:
+
+        if n == 1:
             return nums[0]
+
         def linear(nums):
-            memo={}
-            def f(i):
-                if i>=len(nums):
-                    return 0
-                if i in memo:
-                    return memo[i]
-                rob=nums[i]+f(i+2)
-                skip=f(i+1)
-                memo[i]=max(rob,skip)
-                return memo[i]
-            return f(0)   
-        return max(linear(nums[:n-1]),linear(nums[1:]) )                    
+            n = len(nums)
+
+            if n == 0:
+                return 0
+            if n == 1:
+                return nums[0]
+
+            dp = [0] * n
+            dp[0] = nums[0]
+            dp[1] = max(nums[0], nums[1])
+
+            for i in range(2, n):
+                dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
+
+            return dp[n - 1]
+
+        return max(linear(nums[:n - 1]), linear(nums[1:]))
 
         
